0010bbb47de1bce0ba8026932b0db3c63d0bd6cb
Tommy Volk <tvolk131@gmail.com>
2021-05-15 20:57:45 +0000

    Added show format selection to advanced search

--- client/src/components/advancedSearchMenu.tsx
+++ client/src/components/advancedSearchMenu.tsx
@@ -1,7 +1,45 @@
+import {Checkbox, FormGroup, FormControlLabel, FormLabel, Paper} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 import * as React from 'react';
+import {useState} from 'react';
+
+const useStyles = makeStyles({
+  root: {
+    padding: '15px 5px 5px 15px',
+  }
+});
 
 const AdvancedSearchMenu = () => {
-  return <div>Advanced Search</div>;
+  const [interviewsChecked, setInterviewsChecked] = useState(false);
+  const [presentationsChecked, setPresentationsChecked] = useState(false);
+  const [callInsChecked, setCallInsChecked] = useState(false);
+  const [roundtablesChecked, setRoundtablesChecked] = useState(false);
+
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root} elevation={10}>
+      <FormGroup>
+        <FormLabel>Show Format</FormLabel>
+        <FormControlLabel
+          control={<Checkbox checked={interviewsChecked} onChange={(event) => setInterviewsChecked(event.target.checked)}/>}
+          label={'Interviews'}
+        />
+        <FormControlLabel
+          control={<Checkbox checked={presentationsChecked} onChange={(event) => setPresentationsChecked(event.target.checked)}/>}
+          label={'Presentations'}
+        />
+        <FormControlLabel
+          control={<Checkbox checked={callInsChecked} onChange={(event) => setCallInsChecked(event.target.checked)}/>}
+          label={'Call-Ins'}
+        />
+        <FormControlLabel
+          control={<Checkbox checked={roundtablesChecked} onChange={(event) => setRoundtablesChecked(event.target.checked)}/>}
+          label={'Roundtables'}
+        />
+      </FormGroup>
+    </Paper>
+  );
 };
 
 export default AdvancedSearchMenu;
