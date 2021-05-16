1a4672eba2461a26b442222275c17cea005f7e3b
Tommy Volk <tvolk131@gmail.com>
2021-05-16 18:44:55 +0000

    Search bar also acts as accordion

--- client/src/pages/searchPage.tsx
+++ client/src/pages/searchPage.tsx
@@ -1,9 +1,7 @@
 import {Accordion, AccordionDetails, AccordionSummary, Typography} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 import * as React from 'react';
-import AdvancedSearchMenu from '../components/advancedSearchMenu';
 import SearchBar from '../components/searchBar';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles({
   root: {
@@ -13,10 +11,6 @@ const useStyles = makeStyles({
   nested: {
     maxWidth: 800,
     margin: 'auto'
-  },
-  searchBar: {
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0
   }
 });
 
@@ -26,15 +20,7 @@ const SearchPage = () => {
   return (
     <div className={classes.root}>
       <div className={classes.nested}>
-        <SearchBar className={classes.searchBar} onSearch={(query) => console.log(query)}/>
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
-            <Typography>Advanced Search</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <AdvancedSearchMenu/>
-          </AccordionDetails>
-        </Accordion>
+        <SearchBar onSearch={(query) => console.log(query)}/>
       </div>
     </div>
   );
