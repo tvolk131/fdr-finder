170d360052b4613f1b4c4b11ac5dea52f58ec1d1
Tommy Volk <tvolk131@gmail.com>
2021-05-17 17:42:07 +0000

    Show card renders description in dropdown menu

--- client/src/components/showCard.tsx
+++ client/src/components/showCard.tsx
@@ -1,8 +1,10 @@
-import {Card, CardContent, Typography, Collapse, CardHeader} from '@material-ui/core';
+import {Card, CardContent, Typography, Collapse, CardHeader, CardActions, IconButton} from '@material-ui/core';
+import {ExpandMore as ExpandMoreIcon} from '@material-ui/icons';
 import {makeStyles} from '@material-ui/core/styles';
 import * as React from 'react';
+import {useState} from 'react';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   title: {
     display: 'flex'
   },
@@ -11,8 +13,23 @@ const useStyles = makeStyles({
   },
   audioPlayer: {
     width: '100%'
+  },
+  expand: {
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandClosed: {
+    transform: 'rotate(0deg)'
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  descriptionText: {
+    whiteSpace: 'pre-wrap'
   }
-});
+}));
 
 export enum ShowFormat {
   Interview,
@@ -39,6 +56,8 @@ interface ShowCardProps {
 
 const ShowCard = (props: ShowCardProps) => {
   const classes = useStyles();
+  
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <Card>
@@ -61,6 +80,19 @@ const ShowCard = (props: ShowCardProps) => {
           <source src={props.show.audioLink} type='audio/mpeg'/>
         </audio>
       </CardContent>
+      <CardActions>
+        <IconButton
+          className={`${classes.expand} ${expanded ? classes.expandOpen : classes.expandClosed}`}
+          onClick={() => setExpanded(!expanded)}
+        >
+          <ExpandMoreIcon/>
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
+        <CardContent>
+          <Typography paragraph className={classes.descriptionText}>{props.show.description}</Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
