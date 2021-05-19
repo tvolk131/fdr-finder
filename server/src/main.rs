66965a3effbff5c9d3d4e759b489873f5e0828a7
Tommy Volk <tvolk131@gmail.com>
2021-05-19 17:25:27 +0000

    Added basic api route

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,39 @@
 mod environment;
+mod podcast;
 
 use std::net::SocketAddr;
 use hyper::{http::Error, Body, Request, Response, Server};
+use podcast::Podcast;
 use std::sync::Arc;
 use hyper::service::{make_service_fn, service_fn};
+use environment::EnvironmentVariables;
+use mongodb::{Database, Client, options::FindOptions};
+use bson::Document;
+use futures_lite::StreamExt;
+use serde_json::{Number, Value};
 
 const HTML_BYTES: &'static [u8] = include_bytes!("../../client/out/index.html");
 const JS_BUNDLE_BYTES: &'static [u8] = include_bytes!("../../client/out/bundle.js");
 
-struct HandlerState {}
+struct HandlerState {
+    database: Database
+}
 
 impl HandlerState {
-    fn new() -> Self {
-        HandlerState {}
+    async fn new(env_vars: &EnvironmentVariables) -> Self {
+        HandlerState {
+            database: get_mongo_database_or_panic(env_vars).await
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
+    let env_vars = EnvironmentVariables::new();
     let port = 3000;
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
-    let handler_state = Arc::from(HandlerState::new());
+    let handler_state = Arc::from(HandlerState::new(&env_vars).await);
 
     let make_svc = make_service_fn(move |_| {
         let handler_state = handler_state.clone();
@@ -51,8 +63,38 @@ async fn get_mongo_database_or_panic(env_vars: &EnvironmentVariables) -> Databas
 }
 
 async fn handle_request(req: Request<Body>, handler_state: Arc<HandlerState>) -> Result<Response<Body>, Error> {
-    if req.uri().path().split("/").last().unwrap() == "bundle.js" {
-        return Ok(Response::builder().header("content-type", "application/javascript; charset=utf-8").body(Body::from(JS_BUNDLE_BYTES.to_vec())).unwrap())
+    if req.uri().path().starts_with("/api/") {
+        return handle_api_request(req, handler_state).await;
     }
-    Ok(Response::builder().header("content-type", "text/html; charset=utf-8").body(Body::from(HTML_BYTES.to_vec())).unwrap())
+
+    if req.uri().path().split("/").last().unwrap() == "bundle.js" {
+        return Response::builder().header("content-type", "application/javascript; charset=utf-8").body(Body::from(JS_BUNDLE_BYTES.to_vec()))
+    }
+    Response::builder().header("content-type", "text/html; charset=utf-8").body(Body::from(HTML_BYTES.to_vec()))
+}
+
+async fn handle_api_request(req: Request<Body>, handler_state: Arc<HandlerState>) -> Result<Response<Body>, Error> {
+    if req.uri().path() == "/api/test" {
+        let find_options = FindOptions::builder()
+            // .limit(100)
+            .build();
+        let cursor = handler_state.database.collection("podcasts").find(None, find_options).await.unwrap();
+        let docs_or = cursor
+            .collect::<Vec<Result<Document, mongodb::error::Error>>>()
+            .await;
+        let docs: Vec<Document> = docs_or.into_iter().filter_map(|doc_or| {
+            match doc_or {
+                Ok(doc) => Some(doc),
+                Err(_) => None
+            }
+        }).collect();
+        Value::Number(Number::from(1));
+        let json_array: Vec<Value> = docs.into_iter().map(|doc: Document| {
+            Podcast::from_doc(&doc).to_json()
+        }).collect();
+        let f = Value::Array(json_array);
+        return Response::builder().header("content-type", "application/json").body(Body::from(f.to_string()))
+    }
+
+    Response::builder().status(404).header("content-type", "text/plain").body(Body::from("API endpoint not found"))
 }
