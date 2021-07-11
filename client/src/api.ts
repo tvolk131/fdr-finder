2892ccfdf4fc77171b111eb65d03cd75457c94d4
Tommy Volk <tvolk131@gmail.com>
2021-07-11 18:17:23 +0000

    Client api order matches server route order

--- client/src/api.ts
+++ client/src/api.ts
@@ -9,11 +9,6 @@ const deserializeShowInfo = (data: any): ShowInfo => {
   };
 };
 
-export const searchPodcasts = async (query: string = '', limit: number = 0, skip: number = 0): Promise<ShowInfo[]> => {
-  const res = await axios.get(`/api/search/podcasts?query=${query}&limit=${limit}&skip=${skip}`);
-  return res.data.map(deserializeShowInfo);
-};
-
 export const getPodcast = async (podcastNum: number): Promise<ShowInfo> => {
   return deserializeShowInfo((await axios.get(`/api/podcasts/${podcastNum}`)).data);
 };
@@ -21,6 +16,11 @@ export const getPodcast = async (podcastNum: number): Promise<ShowInfo> => {
 export const getAllPodcasts = async (): Promise<ShowInfo[]> => {
   return (await axios.get('/api/allPodcasts')).data.map(deserializeShowInfo);
 }
+
+export const searchPodcasts = async (query: string = '', limit: number = 0, skip: number = 0): Promise<ShowInfo[]> => {
+  const res = await axios.get(`/api/search/podcasts?query=${query}&limit=${limit}&skip=${skip}`);
+  return res.data.map(deserializeShowInfo);
+};
 
 export const getPodcastRssUrl = (query: string) => {
   let url = 'https://fdr-finder.tommyvolk.com/api/search/podcasts/rss';
