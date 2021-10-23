9523a7b9f3c168a8f2da71828db4550761a4e0d1
Tommy Volk <tvolk131@gmail.com>
2021-10-23 14:25:45 -0500

    Server caches recent searches in LRU (#32)

---
+++ server/src/search/cache.rs
@@ -0,0 +1,60 @@
+use super::meilisearch::SearchResult;
+use crate::podcast::PodcastTag;
+use std::sync::{Arc, Mutex};
+
+type SearchLru = lru::LruCache<(Option<String>, Vec<PodcastTag>), SearchResult>;
+
+pub struct SearchCache {
+    lru: Arc<Mutex<SearchLru>>,
+}
+
+impl SearchCache {
+    pub fn new(cap: usize) -> Self {
+        Self {
+            lru: Arc::from(Mutex::from(lru::LruCache::new(cap))),
+        }
+    }
+
+    pub async fn search(
+        &self,
+        query_or: &Option<String>,
+        tags: &[PodcastTag],
+        limit_or: Option<usize>,
+        mut offset: usize,
+        meilisearch_backend: &super::meilisearch::MeilisearchBackend,
+    ) -> SearchResult {
+        let mut tags_vec = Vec::new();
+        tags_vec.extend_from_slice(tags);
+        {
+            let mut lru = self.lru.lock().unwrap();
+            let cached_result_or = lru.get(&(query_or.clone(), tags_vec.clone()));
+
+            if let Some(cached_result) = cached_result_or {
+                let mut cached_result_clone = cached_result.clone();
+                if offset > 0 {
+                    if offset > cached_result_clone.hits.len() {
+                        offset = cached_result_clone.hits.len();
+                    }
+                    cached_result_clone.hits.drain(0..offset);
+                }
+                if let Some(limit) = limit_or {
+                    cached_result_clone.hits.truncate(limit);
+                };
+                println!("Returning cached hit!");
+                return cached_result_clone;
+            };
+        }
+
+        let result = meilisearch_backend
+            .search(query_or, tags, limit_or.unwrap_or(99999999), offset)
+            .await;
+
+        if limit_or == None && offset == 0 {
+            println!("Caching result.");
+            let mut lru = self.lru.lock().unwrap();
+            lru.put((query_or.clone(), tags_vec), result.clone());
+        }
+
+        result
+    }
+}
